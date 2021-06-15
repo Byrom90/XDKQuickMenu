02257022ceaba680098cb6b4652ee2bf4d2cf38a
Byrom <36423169+Byrom90@users.noreply.github.com>
2021-06-16 00:12:35 +0100

    Add Project files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# XDKQuickMenu
- Notification tray menu for XDK console control
+# XDK Quick Menu
+Notification tray menu for Xbox 360 Devkit console control
+
+### Features
+
+- Power On/Off
+- Open/Close Disc Tray
+
+### General Notes
+**Requires an Xbox 360 Development Kit with Sidecar**
+**Command are sent via the Sidecar USB port**
+
+Creates an icon in the windows notification tray that can be right clicked to activate a ContextMenu providing some basic console control options.
+
+### Additional Requirements
+
+-	Must have the Xbox 360 SDK installed.
+- 	Requires GameDiscEngineV2.interop.dll installed as part of the SDK to be copied to the lib folder of the project. 
+	(This can be found at "C:\Program Files (x86)\Microsoft Xbox 360 SDK\bin\win32")
+
+
+**-Byrom-**
---
+++ XDKQuickPowerMenu/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameDiscEngineV2.Interop;
+
+namespace XDKQuickPowerMenu
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyCustomApplicationContext());
+        }
+    }
+
+    public class MyCustomApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private EmulationServices emulationServices;
+        private IEmulatorConnections emulatorConnections;
+        private IEmulatorConnection emulatorConnection;
+
+        public MyCustomApplicationContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Properties.Resources.XQPicon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Power On", PowerOn),
+                    new MenuItem("Power Off", PowerOff),
+                    //new MenuItem("Reboot", Reboot),
+                    new MenuItem("Open Tray", OpenTray),
+                    new MenuItem("Close Tray", CloseTray),
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+
+            emulationServices = new EmulationServicesClass();
+        }
+
+        bool IsConsoleAvailable()
+        {
+            emulatorConnections = emulationServices.GetEmulatorConnections();
+            if (emulatorConnections.Count != 0)
+            {
+                emulatorConnection = emulatorConnections.Item(0);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Failed to detect XDK console.\nPlease remove & reinsert usb cable and try again.\nNote: Commands are performed via the Sidecar's usb slot");
+                return false;
+            }
+        }
+
+        void PowerOn(object sender, EventArgs e)
+        {
+            if (IsConsoleAvailable())
+                emulatorConnection.SetPower((EmulatorPowerSetting)0);
+        }
+
+        void PowerOff(object sender, EventArgs e)
+        {
+            if (IsConsoleAvailable()) 
+                emulatorConnection.SetPower((EmulatorPowerSetting)1);
+        }
+
+        void Reboot(object sender, EventArgs e)
+        {
+
+        }
+
+        void OpenTray(object sender, EventArgs e)
+        {
+            if (IsConsoleAvailable()) 
+                emulatorConnection.PressTrayEject();
+        }
+        void CloseTray(object sender, EventArgs e)
+        {
+            if (IsConsoleAvailable()) 
+                emulatorConnection.PressTrayEject();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
